e6a3e0b57686bb8662e216edbfcfedc58f98f80f
Evgeny Soynov <esoynov@kogito.network>
2021-04-13 12:24:40 +0200

    Added specs for ufw_route, fixed address and port handling there

--- lib/puppet/provider/ufw_route/ufw_route.rb
+++ lib/puppet/provider/ufw_route/ufw_route.rb
@@ -5,7 +5,7 @@ require 'puppet/resource_api/simple_provider'
 # Implementation for the ufw_route type using the Resource API.
 class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
   def initialize
-    @default_rule_hash = {
+    @default_route_hash = {
       ensure: 'present',
       action: 'reject',
       interface_in: nil,
@@ -25,23 +25,23 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
     context.debug('Returning list of routes')
 
     @instances = []
-    rule_list_lines.each do |line|
+    route_list_lines.each do |line|
       context.debug(line)
-      hash = rule_to_hash(context, line)
+      hash = route_to_hash(context, line)
       @instances << hash unless hash.nil?
       context.warning("Could not parse existing route: #{line}") if hash.nil?
     end
     @instances
   end
 
-  def rule_list_lines
+  def route_list_lines
     result = Puppet::Util::Execution.execute(['/usr/sbin/ufw', 'show', 'added'], failonfail: true)
     result.each_line
           .map(&:strip)
           .select { |line| line.start_with?('ufw route') }
   end
 
-  def rule_to_hash(_context, line)
+  def route_to_hash(_context, line)
     %r{\scomment\s'(?<name>[^']+)'} =~ line
     no_comment = line.sub(%r{\scomment\s'(?<name>[^']+)'}, '')
 
@@ -50,7 +50,7 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
     %r{\sto\s(?<to_addr>[^\s]+)(\s(port|app)\s(?<to_ports_app>[^\s]+))*} =~ no_comment
     %r{\sproto\s(?<proto>\w+)} =~ no_comment
 
-    rule = {
+    route = {
       action: action,
       interface_in: interface_in,
       interface_out: interface_out,
@@ -62,51 +62,51 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
       proto: proto,
     }.delete_if { |_k, v| v.nil? }
 
-    return nil if rule.empty?
+    return nil if route.empty?
 
-    rule[:name] = name.nil? ? Digest::SHA256.hexdigest(no_comment) : name
+    route[:name] = name.nil? ? Digest::SHA256.hexdigest(no_comment) : name
 
-    @default_rule_hash.merge(rule)
+    @default_route_hash.merge(route)
   end
 
-  def rule_to_ufw_params_array(rule)
-    in_definition = rule[:interface_in].nil? ? nil : "in on #{rule[:interface_in]}"
-    out_definition = rule[:interface_out].nil? ? nil : "out on #{rule[:interface_out]}"
+  def route_to_ufw_params_array(route)
+    in_definition = route[:interface_in].nil? ? nil : "in on #{route[:interface_in]}"
+    out_definition = route[:interface_out].nil? ? nil : "out on #{route[:interface_out]}"
 
-    from_addr = rule[:from_addr].nil? ? 'any' : rule[:from_addr]
-    from_checked = "#{from_addr}:#{rule[:from_ports_app]}"
+    from_addr = route[:from_addr].nil? ? 'any' : route[:from_addr]
+    from_checked = "#{from_addr}!#{route[:from_ports_app]}"
     from_definition = case from_checked
-                      when %r{.+:$}
+                      when %r{.+!$}
                         "from #{from_addr}"
-                      when %r{:\d+$}
-                        "from #{from_addr} port #{rule[:from_ports_app]}"
-                      when %r{:\w+$}
-                        "from #{from_addr} app #{rule[:from_ports_app]}"
+                      when %r{![\d,]+$}
+                        "from #{from_addr} port #{route[:from_ports_app]}"
+                      when %r{!\w+$}
+                        "from #{from_addr} app #{route[:from_ports_app]}"
                       end
 
-    to_addr = rule[:to_addr].nil? ? 'any' : rule[:to_addr]
-    to_checked = "#{to_addr}:#{rule[:to_ports_app]}"
+    to_addr = route[:to_addr].nil? ? 'any' : route[:to_addr]
+    to_checked = "#{to_addr}!#{route[:to_ports_app]}"
     to_definition = case to_checked
-                    when %r{.+:$}
+                    when %r{.+!$}
                       "to #{to_addr}"
-                    when %r{:\d+$}
-                      "to #{to_addr} port #{rule[:to_ports_app]}"
-                    when %r{:\w+$}
-                      "to #{to_addr} app #{rule[:to_ports_app]}"
+                    when %r{![\d,]+$}
+                      "to #{to_addr} port #{route[:to_ports_app]}"
+                    when %r{!\w+$}
+                      "to #{to_addr} app #{route[:to_ports_app]}"
                     end
 
     uses_app_name = "#{from_definition} #{to_definition}".include? ' app '
 
-    proto_definition = rule[:proto].nil? ? nil : "proto #{rule[:proto]}"
+    proto_definition = route[:proto].nil? ? nil : "proto #{route[:proto]}"
     proto_definition = nil if uses_app_name # Can't use proto with applications
 
-    comment_definition = rule[:name].nil? ? nil : "comment \'#{rule[:name]}\'"
+    comment_definition = route[:name].nil? ? nil : "comment \'#{route[:name]}\'"
 
     [
-      rule[:action],
+      route[:action],
       in_definition,
       out_definition,
-      rule[:log],
+      route[:log],
       from_definition,
       to_definition,
       proto_definition,
@@ -114,14 +114,14 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
     ].compact
   end
 
-  def rule_to_ufw_params(rule)
-    rule_to_ufw_params_array(rule).join(' ')
+  def route_to_ufw_params(route)
+    route_to_ufw_params_array(route).join(' ')
   end
 
   def create(context, name, should)
     context.notice("Creating '#{name}' with #{should.inspect}")
-    rule = @default_rule_hash.merge(should)
-    params = rule_to_ufw_params(rule)
+    route = @default_route_hash.merge(should)
+    params = route_to_ufw_params(route)
 
     Puppet::Util::Execution.execute("/usr/sbin/ufw route #{params}", failonfail: true)
   end
@@ -129,12 +129,12 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
   def update(context, name, should)
     context.notice("Updating '#{name}' with #{should.inspect}")
     is = @instances.find { |r| r[:name] == name }
-    rule = @default_rule_hash.merge(is).merge(should)
+    route = @default_route_hash.merge(is).merge(should)
 
-    is_params = rule_to_ufw_params(is)
+    is_params = route_to_ufw_params(is)
     Puppet::Util::Execution.execute("/usr/sbin/ufw route delete #{is_params}", failonfail: true)
 
-    params = rule_to_ufw_params(rule)
+    params = route_to_ufw_params(route)
     Puppet::Util::Execution.execute("/usr/sbin/ufw route #{params}", failonfail: true)
   end
 
@@ -142,7 +142,7 @@ class Puppet::Provider::UfwRoute::UfwRoute < Puppet::ResourceApi::SimpleProvider
     context.notice("Deleting '#{name}'")
 
     is = @instances.find { |r| r[:name] == name }
-    params = rule_to_ufw_params(is)
+    params = route_to_ufw_params(is)
     Puppet::Util::Execution.execute("/usr/sbin/ufw route delete #{params}", failonfail: true)
   end
 end
--- spec/unit/puppet/provider/ufw_route/ufw_route_spec.rb
+++ spec/unit/puppet/provider/ufw_route/ufw_route_spec.rb
@@ -26,11 +26,11 @@ RSpec.describe Puppet::Provider::UfwRoute::UfwRoute do
     }
   end
 
-  let(:added_rules) do
+  let(:added_routes) do
     <<-UFW_OUTPUT
-    ufw route allow in on eth0 comment 'wat'
-    ufw route allow in on eth0 from any port 131,132 to 10.0.0.0/24 proto tcp comment 'wat'
-    ufw route allow in on eth0 from any app OpenSSH to 10.5.0.0/24 comment 'wat'
+    ufw route allow in on eth0 comment 'example 1'
+    ufw route allow in on eth0 from any port 131,132 to 10.0.0.0/24 proto tcp comment 'example 2'
+    ufw route deny in on eth0 from any app OpenSSH to 10.5.0.0/24 comment 'example 3'
     UFW_OUTPUT
   end
 
@@ -40,53 +40,125 @@ RSpec.describe Puppet::Provider::UfwRoute::UfwRoute do
     end
   end
 
-  # describe '#get' do
-  #   it 'processes resources' do
-  #     expect(context).to receive(:debug).with('Returning pre-canned example data')
-  #     expect(provider.get(context)).to eq [
-  #       {
-  #         name: 'foo',
-  #         ensure: 'present',
-  #       },
-  #       {
-  #         name: 'bar',
-  #         ensure: 'present',
-  #       },
-  #     ]
-  #   end
-  # end
+  describe '#get' do
+    it 'processes resources' do
+      Puppet::Util::ExecutionStub.set do |_command, _options|
+        added_routes
+      end
+
+      allow(context).to receive(:debug)
+      expect(provider.get(context)).to eq [
+        {
+          name: 'example 1',
+          ensure: 'present',
+          action: 'allow',
+          interface_in: 'eth0',
+          interface_out: nil,
+          log: nil,
+          from_addr: 'any',
+          from_ports_app: nil,
+          to_addr: 'any',
+          to_ports_app: nil,
+          proto: 'any',
+        },
+        {
+          name: 'example 2',
+          ensure: 'present',
+          action: 'allow',
+          interface_in: 'eth0',
+          interface_out: nil,
+          log: nil,
+          from_addr: 'any',
+          from_ports_app: '131,132',
+          to_addr: '10.0.0.0/24',
+          to_ports_app: nil,
+          proto: 'tcp',
+        },
+        {
+          name: 'example 3',
+          ensure: 'present',
+          action: 'deny',
+          interface_in: 'eth0',
+          interface_out: nil,
+          log: nil,
+          from_addr: 'any',
+          from_ports_app: 'OpenSSH',
+          to_addr: '10.5.0.0/24',
+          to_ports_app: nil,
+          proto: 'any',
+        },
+      ]
+    end
+
+    it 'logs existing lines to debug' do
+      Puppet::Util::ExecutionStub.set do |_command, _options|
+        added_routes
+      end
+      expect(context).to receive(:debug).with('Returning list of routes')
+      expect(context).to receive(:debug).with("ufw route allow in on eth0 comment 'example 1'")
+      expect(context).to receive(:debug).with("ufw route allow in on eth0 from any port 131,132 to 10.0.0.0/24 proto tcp comment 'example 2'")
+      expect(context).to receive(:debug).with("ufw route deny in on eth0 from any app OpenSSH to 10.5.0.0/24 comment 'example 3'")
+
+      provider.get(context)
+    end
+  end
 
   describe 'create(context, name, should)' do
     it 'creates the resource' do
+      executed_commands = []
+      Puppet::Util::ExecutionStub.set do |command, _options|
+        executed_commands << command
+      end
+
       expect(context).to receive(:notice).with(%r{\ACreating 'a'})
 
       provider.create(context, 'a', name: 'a', ensure: 'present')
+
+      expect(executed_commands).to eq(['/usr/sbin/ufw route reject from any to any proto any comment \'a\''])
     end
   end
 
   describe 'update(context, name, should)' do
     it 'updates the resource' do
+      executed_commands = []
+      Puppet::Util::ExecutionStub.set do |command, _options|
+        executed_commands << command
+      end
       provider.instance_variable_set(:@instances, [sample_route])
 
       expect(context).to receive(:notice).with(%r{\AUpdating 'foo'})
 
-      provider.update(context, 'foo', name: 'foo', ensure: 'present')
+      provider.update(context, 'foo', name: 'foo', ensure: 'present', interface_in: 'tun3')
+
+      expect(executed_commands).to eq(
+        [
+          '/usr/sbin/ufw route delete allow in on tun0 out on eth1 from any to any proto any comment \'foo\'',
+          '/usr/sbin/ufw route allow in on tun3 out on eth1 from any to any proto any comment \'foo\'',
+        ],
+      )
     end
   end
 
   describe 'delete(context, name)' do
     it 'deletes the resource' do
+      executed_commands = []
+      Puppet::Util::ExecutionStub.set do |command, _options|
+        executed_commands << command
+      end
+
       provider.instance_variable_set(:@instances, [sample_route])
 
       expect(context).to receive(:notice).with(%r{\ADeleting 'foo'})
 
       provider.delete(context, 'foo')
+
+      expect(executed_commands).to eq(['/usr/sbin/ufw route delete allow in on tun0 out on eth1 from any to any proto any comment \'foo\''])
     end
   end
 
-  describe 'rule_to_hash(context, line)' do
+  describe 'route_to_hash(context, line)' do
     it 'parses line with basic parameters' do
-      expect(provider.rule_to_hash(nil, "ufw route allow in on eth0 comment 'test 1'")).to eq(
+      expect(provider.route_to_hash(nil, "ufw route allow in on eth0 comment 'test 1'")).to eq(
         {
           ensure: 'present',
           name: 'test 1',
@@ -103,22 +175,130 @@ RSpec.describe Puppet::Provider::UfwRoute::UfwRoute do
       )
     end
 
-    # it 'parses line in short syntax' do
-    #   expect(provider.rule_to_hash(nil, "ufw allow log 555,777/tcp comment 'test 1'")).to eq(
-    #     {
-    #       ensure: 'present',
-    #       name: 'test 1',
-    #       action: 'allow',
-    #       direction: 'in',
-    #       interface: nil,
-    #       log: nil,
-    #       from_addr: '10.5.3.0/24',
-    #       from_ports_app: nil,
-    #       to_addr: 'any',
-    #       to_ports_app: nil,
-    #       proto: 'gre',
-    #     }
-    #   )
-    # end
+    it 'correctly parses comments with keywords' do
+      expect(provider.route_to_hash(nil, "ufw route allow in on eth0 comment 'ufw allow out on eth1 from 10.1.3.3 port 3133 to 10.3.3.3 port 2122'")).to eq(
+        {
+          ensure: 'present',
+          name: 'ufw allow out on eth1 from 10.1.3.3 port 3133 to 10.3.3.3 port 2122',
+          action: 'allow',
+          interface_in: 'eth0',
+          interface_out: nil,
+          log: nil,
+          from_addr: 'any',
+          from_ports_app: nil,
+          to_addr: 'any',
+          to_ports_app: nil,
+          proto: 'any',
+        },
+      )
+    end
+  end
+
+  describe 'route_to_ufw_params(route)' do
+    it 'converts minimal route to string' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow' })).to eq('allow from any to any')
+    end
+
+    it 'converts full route to string' do
+      expect(provider.route_to_ufw_params(
+        {
+          'action': 'reject',
+          'interface_in': 'tun0',
+          'interface_out': 'eth0',
+          'log': 'log-all',
+          'from_addr': '10.1.0.0/24',
+          'from_ports_app': '8080,8081',
+          'to_addr': '2001:db8:1234::/48',
+          'to_ports_app': '3131',
+          'proto': 'udp',
+        },
+      )).to eq('reject in on tun0 out on eth0 log-all from 10.1.0.0/24 port 8080,8081 to 2001:db8:1234::/48 port 3131 proto udp')
+    end
+
+    it 'handles any in from_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': 'any' })).to eq('allow from any to any')
+    end
+
+    it 'handles any in to_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': 'any' })).to eq('allow from any to any')
+    end
+
+    it 'handles ipv4 address in from_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '10.1.0.0' })).to eq('allow from 10.1.0.0 to any')
+    end
+
+    it 'handles ipv4 network in from_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '10.1.0.0/24' })).to eq('allow from 10.1.0.0/24 to any')
+    end
+
+    it 'handles ipv6 address in from_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '2606:4700:4700::1111' })).to eq('allow from 2606:4700:4700::1111 to any')
+    end
+
+    it 'handles ipv6 network in from_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '2001:db8:1234::/48' })).to eq('allow from 2001:db8:1234::/48 to any')
+    end
+
+    it 'handles ipv6 address in from_addr with port' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '2606:4700:4700::1111', 'from_ports_app': 8080 })).to eq('allow from 2606:4700:4700::1111 port 8080 to any')
+    end
+
+    it 'handles ipv6 network in from_addr with port' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_addr': '2001:db8:1234::/48', 'from_ports_app': 8080 })).to eq('allow from 2001:db8:1234::/48 port 8080 to any')
+    end
+
+    it 'handles ipv4 address in to_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '10.1.0.0' })).to eq('allow from any to 10.1.0.0')
+    end
+
+    it 'handles ipv4 network in to_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '10.1.0.0/24' })).to eq('allow from any to 10.1.0.0/24')
+    end
+
+    it 'handles ipv6 address in to_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '2606:4700:4700::1111' })).to eq('allow from any to 2606:4700:4700::1111')
+    end
+
+    it 'handles ipv6 network in to_addr' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '2001:db8:1234::/48' })).to eq('allow from any to 2001:db8:1234::/48')
+    end
+
+    it 'handles ipv6 address in to_addr with port' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '2606:4700:4700::1111', 'to_ports_app': 8080 })).to eq('allow from any to 2606:4700:4700::1111 port 8080')
+    end
+
+    it 'handles ipv6 network in to_addr with port' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_addr': '2001:db8:1234::/48', 'to_ports_app': 8080 })).to eq('allow from any to 2001:db8:1234::/48 port 8080')
+    end
+
+    it 'handles comma separated ports in from_ports_app' do
+      expect(provider.route_to_ufw_params(
+        {
+          'action': 'allow',
+          'from_addr': '2606:4700:4700::1111',
+          'from_ports_app': '8080,8081'
+        },
+      )).to eq('allow from 2606:4700:4700::1111 port 8080,8081 to any')
+    end
+
+    it 'handles comma separated ports in to_ports_app' do
+      expect(provider.route_to_ufw_params(
+        {
+          'action': 'allow',
+          'to_addr': '2606:4700:4700::1111',
+          'to_ports_app': '8080,8081'
+        },
+      )).to eq('allow from any to 2606:4700:4700::1111 port 8080,8081')
+    end
+
+    it 'does not add proto when app is specified in from_ports_app or to_ports_app' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_ports_app': 'OpenSSH', 'proto' => 'tcp' })).to eq('allow from any app OpenSSH to any')
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'to_ports_app': 'OpenSSH', 'proto' => 'tcp' })).to eq('allow from any to any app OpenSSH')
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'from_ports_app': 'OpenSSH', 'to_ports_app': 'OpenSSH', 'proto' => 'tcp' })).to eq('allow from any app OpenSSH to any app OpenSSH')
+    end
+
+    it 'adds proto if specified without app' do
+      expect(provider.route_to_ufw_params({ 'action': 'allow', 'proto': 'tcp' })).to eq('allow from any to any proto tcp')
+    end
   end
 end
